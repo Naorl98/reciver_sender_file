c519295b4ff43b49b2b64b321a648ac134193abf
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-12-16 06:38:35 +0200

    Update Reciver.c

--- Reciver.c
+++ Reciver.c
@@ -16,6 +16,7 @@ void listenTo(int client_Socket){
     int len;
     int index = 0; // printe times loop index
     char buffer[BUFSIZ];// the message from the client
+    char sizefile[256];
     int size =0;
     double avarageTime=0; // save the avarage time of each part
     int partSize1 = 0; // save part1 size
@@ -35,61 +36,80 @@ void listenTo(int client_Socket){
 
   
     for(; ;) // infinity loop
-    {
-        bzero(buffer, BUFSIZ); //reset buffer
-        recv(client_Socket,buffer,BUFSIZ,0); // recivre message from client and save in buffer
-        size = atoi(buffer); // cast from char to int
+    {   
+
+        recv(client_Socket,sizefile,256,0); // recivre message from client and save in buffer
+        size = atoi(sizefile); // cast from char to int
         if(size == -2) { // if client want to exit
             index=0;
             printf("cubic:\n");
-            while(part1times[index] != 0 && index <= fileIndex){ 
+            while(index < fileIndex){ 
                 avarageTime+=part1times[index]; // clculate all the time of the first parts
                 printf("%f \n",part1times[index]); // print all the times of the first part
                 index++;
             }
-            avarageTime = avarageTime/(index+1); // calculate the avarage time of first parts
+            avarageTime = avarageTime/(fileIndex); // calculate the avarage time of first parts
             printf("The avarage time is: %f \n",avarageTime); // prints the avarage times
             index =0; // reset 
             avarageTime=0; //reset
             printf("reno:\n");
-            while(part2times[index]!= 0 && index <= fileIndex){ 
+            while( index < fileIndex){ 
                 avarageTime+=part2times[index];// clculate all the time of the second parts
                 printf("%f \n",part2times[index]);// print all the times of the second part
                 index++;
             }
-            avarageTime = avarageTime/(index+1);// calculate the avarage time of second parts
+            avarageTime = avarageTime/(fileIndex);// calculate the avarage time of second parts
             printf("The avarage time is: %f \n",avarageTime);  // prints the avarage times
             fclose(fileRecived); // close the file
             break;
-
         }
-        partSize1 = size/2; //calculate the first part size
-        partSize2 = size - partSize1;//calculate the second part size
-        
-        fileRecived = fopen(NAME, "w"); // open new file
-        
+        fileRecived = fopen(NAME, "w"); // open new file  
         if(fileRecived == NULL) // check if open succeed
         {
             printf("open file - failed\n");
             exit(0);
         }
+        else printf("file crated\n");
+        partSize1 = size/2; //calculate the first part size
+        partSize2 = size - partSize1;//calculate the second part size
         time = clock();// save start recive time
-        while((partSize1>0)&&((length = recv(client_Socket,buffer,BUFSIZ,0))>0)){// get first part of file 
-            fwrite(buffer, sizeof(char), length, fileRecived); // write to the new file
+        while((partSize1>0)){// get first part of file 
+            length = recv(client_Socket,buffer,BUFSIZ,0);// recive data
+            if(length == -1){
+                printf("eror - recive data");
+                exit(0);
+            }
+            if(fwrite(buffer, sizeof(char), length, fileRecived) == -1){// write to the new file
+                printf("eror - write on file");
+                exit(0);
+            } 
             partSize1 -= length;
+
         }
         time = clock() - time; // save recived time
         part1times[fileIndex] = ((double)time)/CLOCKS_PER_SEC;; // save the time in seconds
-        write(client_Socket, buf, 16); // send XOR to client
+        send(client_Socket, buf, 16,0);// send XOR to client
         if(setsockopt(client_Socket,IPPROTO_TCP,TCP_CONGESTION,"reno",4)<0){ // change the cc algorithem
                 printf("eror changing cc\n");
                 exit(0);
             }
         else printf("change cc - reno\n");
         time = clock(); // save start recive time
-        while((partSize2>0)&&((length = recv(client_Socket,buffer,BUFSIZ,0))>0)){ // get second part of file
-            fwrite(buffer, sizeof(char), length, fileRecived); // write to the file
+        while((partSize2>0)){ // get second part of file
+            length = recv(client_Socket,buffer,BUFSIZ,0);//recive data
+            if(length == -1){
+                printf("eror - recive data");
+                exit(0);
+            }
+            if(fwrite(buffer, sizeof(char), length, fileRecived) == -1){ // write to the file
+                printf("eror - write data");
+                exit(0);
+            }
             partSize2 -= length;
+            if(partSize2==0){
+                partSize2= 0 ;
+            }
+
         }
         time = clock() - time; // save recived time
         part2times[fileIndex] +=((double)time)/CLOCKS_PER_SEC;// save time in second
